25887544f83a57571fe0ab11a942591410d48138
Darrick Fauvel <76695983+DarrickFauvel@users.noreply.github.com>
2025-01-31 00:26:49 +0000

    add allowed origins to next config

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  experimental: {
+    serverActions: {
+      allowedOrigins: ['localhost:3000', 'stunning-engine-w4979444jv6hx95-3000.app.github.dev'],
+    },
+  },
 };
 
 export default nextConfig;
+
